c710798feadc93a769f94798fe882562e586ba5b
xuhui <xuhui@simpletour.com>
2016-04-26 17:04:55 +0800

    BaseRepository更改为多继承

--- dao/src/main/java/me/afsd/dao/base/BaseRepository.java
+++ dao/src/main/java/me/afsd/dao/base/BaseRepository.java
@@ -1,29 +1,17 @@
 package me.afsd.dao.base;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
-import java.util.List;
 
 /**
- * @author  XuHui (416422546@qq.com)
+ * @author XuHui (416422546@qq.com)
  * @version 0.0.1
  */
 @NoRepositoryBean
 public interface BaseRepository<T, ID extends Serializable>
-        extends JpaRepository<T, ID> {
-
-    Page<T> findAll(Specification<T> spec,Pageable pageable);
-
-    List<T> findAll(Specification<T> spec, Sort sort);
-
-    List<T> findAll(Specification<T> spec);
-
+        extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
     String getDomainName();
-
 }
